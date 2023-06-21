63375207184e00d3278764587e9de90c610876db
Amy Tisdell <amytisdell@icloud.com>
2023-06-20 22:10:59 -0400

    functional API calls and basic bootstrap styling

---
+++ index.js
@@ -0,0 +1,32 @@
+const url =
+  "https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=word";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ff192d86f2mshc5617dbfd1b22e9p154e8djsn8b804a1cb9ee",
+    "X-RapidAPI-Host": "dictionary-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+submitWord = async function () {
+  const word = document.getElementById("word").value;
+  const url = `https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=${word}`;
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
+  const resultParse = JSON.parse(result)
+    .definition.replace("2.", "<br>2.")
+    .replace("3.", "<br>3.")
+    .replace("4.", "<br>4.")
+    .replace("5.", "<br>5.")
+    .replace("6.", "<br>6.")
+    .replace("/n", "<br>");
+  const capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+
+  let resultText = document.getElementById("resultText");
+  let resultWord = document.getElementById("wordResult");
+
+  resultWord.innerHTML = `<H4>Word:</h4> ${capitalized}`;
+  resultText.innerHTML = `<h4>Defintion:</h4> ${resultParse}`;
+  document.getElementById("word").value = "";
+};
